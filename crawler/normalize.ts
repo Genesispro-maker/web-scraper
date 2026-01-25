ce4dee6eb280ed14b17a5dfc08311b2f458e0baf
Genesispro-maker <genesisprog894@gmail.com>
2026-01-25 10:23:20 -0800

    Update normalize.ts

--- crawler/normalize.ts
+++ crawler/normalize.ts
@@ -1,4 +1,10 @@
 export function NormalizeURLS(url: string){
     const urlobject = new URL(url)
     let fullURL = `${urlobject.host}${urlobject.pathname}`
+
+    if(fullURL.slice(0, -1) === "/"){
+        fullURL = fullURL.slice(0, -1)
+    }
+
+    return fullURL
 }
